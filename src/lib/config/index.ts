d2fbfc708b346f82c79147ecca14cde551a6e966
Nigel <delvieronigel@gmail.com>
2023-04-18 14:28:39 +0700

    Changed endpoint

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -19,7 +19,7 @@ export const currencyCode = 'USD'
 export const currencySymbol = '$'
 export const DOMAIN = env.PUBLIC_DOMAIN || 'http://localhost:3000'
 export const entity = 'Misiki'
-export const HTTP_ENDPOINT = 'https://2cc7-103-105-33-114.ngrok-free.app/v1/'
+export const HTTP_ENDPOINT = 'https://api.mayar.club/hl/v1/'
 export const IMAGE_CDN_URL = 'https://ik.imagekit.io/3wzatecz51w3i'
 export const mediaqueries = {
 	xs: '(min-width: 480px)',
